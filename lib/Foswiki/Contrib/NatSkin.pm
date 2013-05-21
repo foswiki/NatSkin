60a84621908ca3a3aa7c20143bd2e01c53e80e36
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-05-21 07:18:27 +0000

    Item12500: working around inefficiencies of core to fetch the author's name of an initial revision by using the DBCachePlugin equivalent if installed
    git-svn-id: http://svn.foswiki.org/trunk/NatSkin@16739 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/NatSkin.pm
+++ lib/Foswiki/Contrib/NatSkin.pm
@@ -3,8 +3,8 @@ package Foswiki::Contrib::NatSkin;
 use strict;
 use warnings;
 
-our $VERSION = '3.99_007';
-our $RELEASE = '3.99_007';
+our $VERSION = '3.99_008';
+our $RELEASE = '3.99_008';
 our $SHORTDESCRIPTION = 'Modern web design for Foswiki';
 
 1;
