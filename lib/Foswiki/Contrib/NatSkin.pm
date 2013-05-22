4e0938e912db1fd8ad41f43c0acd2c638a17441f
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-05-22 18:31:09 +0000

    Item12481: fixed grid cols with a border wrapping on small viewports
    git-svn-id: http://svn.foswiki.org/trunk/NatSkin@16743 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/NatSkin.pm
+++ lib/Foswiki/Contrib/NatSkin.pm
@@ -3,8 +3,8 @@ package Foswiki::Contrib::NatSkin;
 use strict;
 use warnings;
 
-our $VERSION = '3.99_008';
-our $RELEASE = '3.99_008';
+our $VERSION = '3.99_009';
+our $RELEASE = '3.99_009';
 our $SHORTDESCRIPTION = 'Modern web design for Foswiki';
 
 1;
