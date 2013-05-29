9ec96ff65345871ae6f8909f58169704aeeccb54
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-05-29 10:12:15 +0000

    Item12481: up'ed dependency on latest JQueryPlugin
    git-svn-id: http://svn.foswiki.org/trunk/NatSkin@16752 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/NatSkin.pm
+++ lib/Foswiki/Contrib/NatSkin.pm
@@ -3,8 +3,8 @@ package Foswiki::Contrib::NatSkin;
 use strict;
 use warnings;
 
-our $VERSION = '3.99_011';
-our $RELEASE = '3.99_011';
+our $VERSION = '3.99_012';
+our $RELEASE = '3.99_012';
 our $SHORTDESCRIPTION = 'Modern web design for Foswiki';
 
 1;
