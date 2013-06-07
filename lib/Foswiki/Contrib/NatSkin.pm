03aadce474d7333dd3ef820f5a39627748680a15
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-06-07 14:04:38 +0000

    Item12481: docu work
    git-svn-id: http://svn.foswiki.org/trunk/NatSkin@16797 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/NatSkin.pm
+++ lib/Foswiki/Contrib/NatSkin.pm
@@ -3,8 +3,8 @@ package Foswiki::Contrib::NatSkin;
 use strict;
 use warnings;
 
-our $VERSION = '3.99_013';
-our $RELEASE = '3.99_013';
+our $VERSION = '3.99_014';
+our $RELEASE = '3.99_014';
 our $SHORTDESCRIPTION = 'Modern web design for Foswiki';
 
 1;
