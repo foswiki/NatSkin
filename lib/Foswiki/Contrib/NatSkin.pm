1e113d8c48f9deb8001b4993ddb10c0b2fa394ec
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-06-07 16:50:55 +0000

    Item12481: fixed dropdown menu in topic actions
    git-svn-id: http://svn.foswiki.org/trunk/NatSkin@16798 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/NatSkin.pm
+++ lib/Foswiki/Contrib/NatSkin.pm
@@ -3,8 +3,8 @@ package Foswiki::Contrib::NatSkin;
 use strict;
 use warnings;
 
-our $VERSION = '3.99_014';
-our $RELEASE = '3.99_014';
+our $VERSION = '3.99_015';
+our $RELEASE = '3.99_015';
 our $SHORTDESCRIPTION = 'Modern web design for Foswiki';
 
 1;
