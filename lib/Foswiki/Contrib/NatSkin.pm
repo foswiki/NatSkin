83c1da256ad7d996764694e4960d21acfd1feb2e
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-06-19 13:46:17 +0000

    Item12481:
       * removed dependency on SetVariablePlugin
       * some css improvements
    
    
    
    git-svn-id: http://svn.foswiki.org/trunk/NatSkin@16806 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/NatSkin.pm
+++ lib/Foswiki/Contrib/NatSkin.pm
@@ -3,8 +3,8 @@ package Foswiki::Contrib::NatSkin;
 use strict;
 use warnings;
 
-our $VERSION = '3.99_015';
-our $RELEASE = '3.99_015';
+our $VERSION = '3.99_016';
+our $RELEASE = '3.99_016';
 our $SHORTDESCRIPTION = 'Modern web design for Foswiki';
 
 1;
