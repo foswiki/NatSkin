a34a42f85d30747c298e1afa72ab0f42f9f2b3d0
MichaelDaum <github@michaeldaumconsulting.com>
2015-02-16 12:45:47 +0100

    Item12481: adding screenshots to repo
    - fixing margins of top panel in fluid layout mode
    - add more margins to h2,h3,h4
    - remove deprecated html attrs as spotted
    - hide logout link when not applicable
    - hide topic actions in static content

--- lib/Foswiki/Contrib/NatSkin.pm
+++ lib/Foswiki/Contrib/NatSkin.pm
@@ -3,8 +3,8 @@ package Foswiki::Contrib::NatSkin;
 use strict;
 use warnings;
 
-our $VERSION = '3.99_023';
-our $RELEASE = '3.99_023';
+our $VERSION = '3.99_024';
+our $RELEASE = '3.99_024';
 our $SHORTDESCRIPTION = 'Modern web design for Foswiki';
 
 1;
